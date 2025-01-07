15fdecf1dc1c57030a373cc69fdc5a7927f64e7d
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-01-07 14:28:05 -0300

    add .env api key, fix build

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = "https://ljmuomsfouaxhinmmuwx.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqbXVvbXNmb3VheGhpbm1tdXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxODE3MzEsImV4cCI6MjA1MTc1NzczMX0.LpH-pCTJwBIYYdEACcr3JHBnLXPZiAcE7Z_1VfS0qiw";
+const supabaseKey = process.env.SUPABASE_SECRET_API_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
