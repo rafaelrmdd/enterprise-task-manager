ff499c7b454e62ec0e3060601e179d0c472262b4
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-01-07 15:40:38 -0300

    fix build 3

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,7 +1,6 @@
 import Content from "../components/dashboard/Content"
 import Sidebar from "../components/SideBar";
 
-
 export default function Workspace() {
     return (
         <div className="flex flex-row h-screen">
--- frontend/src/pages/tasks/index.jsx
+++ frontend/src/pages/tasks/index.jsx
@@ -2,7 +2,6 @@ import Sidebar from "../../components/SideBar";
 import Content from "../../components/tasks/Content";
 
 export default function Tasks() {
-
     return (
         <div className="flex flex-row h-screen">
             <Sidebar />
